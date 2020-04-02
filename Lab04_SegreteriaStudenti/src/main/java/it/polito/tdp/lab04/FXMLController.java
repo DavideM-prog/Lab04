480f500d2477729905ed1340c76ad16a7844e605
Matteo <Matteo@Luca>
2020-04-02 17:27:41 +0200

    creazione parte grafica

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -1,23 +1,97 @@
 /**
  * Sample Skeleton for 'Scene.fxml' Controller Class
  */
-
 package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private ComboBox<?> boxCorso;
+
+    @FXML
+    private Button btnCercaIscritti;
+
+    @FXML
+    private TextField txtMatricola;
+
+    @FXML
+    private Button btnMostraStudente;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtCognome;
+
+    @FXML
+    private Button btnCercaCorsiStudente;
+
+    @FXML
+    private Button btnIscrivi;
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    void doActivation(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaCorsiStudente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaIscrittiCorso(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doIscriviStudente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostraStudente(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert boxCorso != null : "fx:id=\"boxCorso\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaIscritti != null : "fx:id=\"btnCercaIscritti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtMatricola != null : "fx:id=\"txtMatricola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnMostraStudente != null : "fx:id=\"btnMostraStudente\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtNome != null : "fx:id=\"txtNome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCognome != null : "fx:id=\"txtCognome\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaCorsiStudente != null : "fx:id=\"btnCercaCorsiStudente\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnIscrivi != null : "fx:id=\"btnIscrivi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
